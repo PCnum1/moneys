fa893e690473932ae60043dbafe0a126572bdde9
PCnum1 <1284535166@qq.com>
2021-03-10 15:17:36 +0800

    deploy

---
+++ precache-manifest.057b117b847abb7d50590a0eca9eae2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc72eb78113200a6268",
+    "url": "/moneys/css/app.38ccfae4.css"
+  },
+  {
+    "revision": "021b8247139010a1b2ecdeb421cbd096",
+    "url": "/moneys/index.html"
+  },
+  {
+    "revision": "2fc72eb78113200a6268",
+    "url": "/moneys/js/app.9cf88298.js"
+  },
+  {
+    "revision": "2de0a379d67f2f252acf",
+    "url": "/moneys/js/chunk-vendors.a7c50486.js"
+  },
+  {
+    "revision": "11793ed133f48c5608453a02bff3309e",
+    "url": "/moneys/manifest.json"
+  },
+  {
+    "revision": "854c33d2d8860f70c4ece1191c96a90e",
+    "url": "/moneys/qcurp.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/moneys/robots.txt"
+  }
+]);
